2313e3ba41814d7df81b99c38badfe641a08b3b9
Anton Toneyev <aatoneyev@gmail.com>
2022-09-27 17:06:10 +0300

    action bar color changes

--- app/src/main/java/com/anton/lspu/account/DrawerActivity.java
+++ app/src/main/java/com/anton/lspu/account/DrawerActivity.java
@@ -82,6 +82,7 @@ public class DrawerActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.drawer, menu);
 
+
         return true;
     }
 
